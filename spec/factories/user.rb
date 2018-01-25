ab6341915efed0e6f103c645424cedf8be8cb8ab
yoruno07 <ryo.yoru.io@gmail.com>
2018-01-26 00:33:40 +0900

    del user.rb

--- spec/factories/user.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    name "ユーザーテスト"
-    age 30
-  end
-end
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    name "ユーザーテスト"
+    age 30
+  end
+end
